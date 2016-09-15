d42501fe44558732a637d7f6a43cd07ebed90b41
Nathan Hur <hur.nathan@gmail.com>
2016-09-15 20:33:54 +1200

    Add level selection, center layout, hover styles

--- src/voxspell/Controller.java
+++ src/voxspell/Controller.java
@@ -1,15 +1,19 @@
 package voxspell;
 
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+    private static String ON_HOVER = "-fx-background-color: #83B496";
+    private static String ON_EXIT = "-fx-background-color: #b6e7c9";
+    private ArrayList<Button> buttons = new ArrayList<Button>();
 
     @FXML
     private Button level1;
@@ -32,31 +36,61 @@ public class Controller implements Initializable {
     @FXML
     private Button level10;
 
+    // add buttons to list
+
     private int getLevelNumber(String text) {
         String number = text.substring(text.length() - 1);
+        if (number.equals("0")) {
+            return 10;
+        }
         return Integer.parseInt(number);
     }
 
-    class levelSelect implements EventHandler<ActionEvent> {
-        public void handle(ActionEvent event) {
+    class levelSelect implements EventHandler<MouseEvent> {
+        public void handle(MouseEvent event) {
             String text = ((Button)event.getSource()).getText();
             int level = getLevelNumber(text);
+            System.out.println("Going to level " + level);
             // TODO use level to get wordlist and move to spelling scene
+        }
+    }
+
+    class hoverHandler implements EventHandler<MouseEvent> {
+        public void handle(MouseEvent event) {
+            ((Button)event.getSource()).setStyle(ON_HOVER);
+        }
+    }
+
+    class exitHandler implements EventHandler<MouseEvent> {
+        public void handle(MouseEvent event) {
+            ((Button)event.getSource()).setStyle(ON_EXIT);
         }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        EventHandler<ActionEvent> level = new levelSelect();
-        level1.setOnAction(level);
-        level2.setOnAction(level);
-        level3.setOnAction(level);
-        level4.setOnAction(level);
-        level5.setOnAction(level);
-        level6.setOnAction(level);
-        level7.setOnAction(level);
-        level8.setOnAction(level);
-        level9.setOnAction(level);
-        level10.setOnAction(level);
+
+        // create listeners
+        EventHandler<MouseEvent> levelSelectionHandler = new levelSelect();
+        EventHandler<MouseEvent> hoverHandler = new hoverHandler();
+        EventHandler<MouseEvent> exitHandler = new exitHandler();
+
+        // add buttons to list, then iterate through list assigning listeners
+        buttons.add(level1);
+        buttons.add(level2);
+        buttons.add(level3);
+        buttons.add(level4);
+        buttons.add(level5);
+        buttons.add(level6);
+        buttons.add(level7);
+        buttons.add(level8);
+        buttons.add(level9);
+        buttons.add(level10);
+
+        for (Button button : buttons) {
+            button.setOnMouseClicked(levelSelectionHandler);
+            button.setOnMouseEntered(hoverHandler);
+            button.setOnMouseExited(exitHandler);
+        }
     }
 }
--- src/voxspell/Voxspell.java
+++ src/voxspell/Voxspell.java
@@ -6,18 +6,37 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.lang.reflect.Field;
+import java.util.HashMap;
+
 public class Voxspell extends Application {
+    private static int WINDOW_WIDTH = 800;
+    private static int WINDOW_HEIGHT = 600;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("VOXSPELL");
+        primaryStage.setScene(new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
+        /**
+         * START OF MAC WORKAROUND TODO: remove when finished
+         */
+        try {
+            Class<?> macFontFinderClass = Class.forName("com.sun.t2k.MacFontFinder");
+            Field psNameToPathMap = macFontFinderClass.getDeclaredField("psNameToPathMap");
+            psNameToPathMap.setAccessible(true);
+            psNameToPathMap.set(null, new HashMap<String, String>());
+        } catch (Exception e) {
+            // ignore
+        }
+        /**
+         * END OF MAC WORKAROUND
+         */
         launch(args);
     }
 }
