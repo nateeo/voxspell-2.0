c9f6293c5d107cfd318b9e34115cc9f3ff9f099a
harrylimp <harrylim.liverpool@gmail.com>
2016-09-21 18:38:28 +1200

    Add stats piechart and listview

--- src/voxspell/scenes/SessionController.java
+++ src/voxspell/scenes/SessionController.java
@@ -26,9 +26,10 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /**
- * Created by harrylimp on 19/09/16.
+ * Created by harrylimp on 21/09/16.
  */
-public class StatsController implements Initializable {
+public class SessionController implements Initializable {
+
 
     @FXML
     private Button reviewButton;
@@ -36,10 +37,12 @@ public class StatsController implements Initializable {
     private Button returnButton;
     @FXML
     private PieChart piechart;
+    @FXML
+    private ListView<String> listView;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        returnButton.setOnMouseClicked(new returnHandler());
+        returnButton.setOnMouseClicked(new SessionController.returnHandler());
 
         //showPieChart();
         //showListView();
@@ -75,11 +78,43 @@ public class StatsController implements Initializable {
                 incorrect++;
             }
         }
-        ObservableList<javafx.scene.chart.PieChart.Data> list = FXCollections.observableArrayList(
+        ObservableList<PieChart.Data> list = FXCollections.observableArrayList(
                 new javafx.scene.chart.PieChart.Data("Correct", correct * 10),
                 new javafx.scene.chart.PieChart.Data("Incorrect", incorrect * 10)
         );
         piechart.setData(list);
     }
 
+    public void showListView() {
+        ArrayList<Word> currentWords = LevelData.getCurrentWordList();
+        ListView<String> list = new ListView<>();
+        ObservableList<String> words = FXCollections.observableArrayList();
+        for (Word word : currentWords) {
+            words.add(word.toString());
+        }
+        list.setItems(words);
+        list.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+            class ColorRectCell extends ListCell<String> {
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    Rectangle rect = new Rectangle(100, 20);
+                    int position = LevelData.getCurrentWordList().indexOf(item);
+                    Word word = LevelData.getCurrentWordList().get(position);
+                    if (word.getMastered() == 1) {
+                        rect.setFill(Color.GREEN);
+                        setGraphic(rect);
+                    } else {
+                        rect.setFill(Color.RED);
+                        setGraphic(rect);
+                    }
+                }
+            }
+
+            @Override
+            public ListCell<String> call(ListView<String> list) {
+                return new ColorRectCell();
+            }
+        });
+    }
 }
