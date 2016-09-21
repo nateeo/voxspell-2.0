b123fb3ee4418ff6249d279e5b563e027034d259
Nathan Hur <hur.nathan@gmail.com>
2016-09-21 18:01:57 +1200

    Add return button to stats

--- src/voxspell/scenes/StatsController.java
+++ src/voxspell/scenes/StatsController.java
@@ -2,23 +2,33 @@ package voxspell.scenes;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 import voxspell.engine.LevelData;
 import voxspell.engine.Word;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 /**
  * Created by harrylimp on 19/09/16.
  */
-public class StatsController {
+public class StatsController implements Initializable {
 
     @FXML
     private Button reviewButton;
@@ -28,6 +38,30 @@ public class StatsController {
     private PieChart piechart;
     @FXML
     private ListView<String> listView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        returnButton.setOnMouseClicked(new returnHandler());
+    }
+
+    class returnHandler implements EventHandler<MouseEvent> {
+
+        @Override
+        public void handle(MouseEvent mouseEvent) {
+            Stage stage;
+            Parent root = null;
+            stage = (Stage) ((Button)mouseEvent.getSource()).getScene().getWindow();
+
+            try {
+                root = FXMLLoader.load(getClass().getResource("main.fxml"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+    }
 
     public void showPieChart() {
         ArrayList<Word> currentWords = LevelData.getCurrentWordList();
